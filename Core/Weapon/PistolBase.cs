8f3f9d6014172b3c6e57ccd52d138d6301f78aad
Sniky <sniky83@hotmail.fr>
2024-06-25 21:34:15 +0200

    PistolBase -> Abstract class

--- Core/Weapon/PistolBase.cs
+++ Core/Weapon/PistolBase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core.Weapon
 {
-    public class PistolBase : WeaponBase
+    public abstract class PistolBase : WeaponBase
     {
         public PistolBase(string name, double recoil, int weight, int damage, int ammo = 8, bool isPistol = true) : base(name, recoil, weight, damage, ammo, isPistol) { }
     }
